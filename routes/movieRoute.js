1ca7f5f526d657dde9f1ca4f3b356ba968c3b0b2
n0e <noemy.garcia@gmail.com>
2022-03-22 11:25:14 +0100

    [ADD] getMovie in movieController

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -6,7 +6,7 @@ const movie = require('../controllers/movieController');
 
 // [GET] http://localhost:3000/api/film/:title 
 // Retorna un JSON con los detalles de la peli en concreto buscada. Payload {titulo:"Titanic", autor:"James Cameron", descripción:"xxx", src:"titanic.png",etc...}
-router.get('/film', movie.getMovie);
+router.get('/film/:title', movie.getMovie);
 
 // [POST] http://localhost:3000/api/film/ 
 // Se envía por POST los datos de la película a crear y retorna un status 200. Payload {message: "Se ha guardado Titanic"}
